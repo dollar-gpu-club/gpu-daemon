b5d37598ef013b7933b769cbd45500d9d048f3c0
Colin King <colin.king@segment.com>
2018-10-06 13:56:39 -0700

    Add GPU daemon

--- src/index.js
+++ src/index.js
@@ -1,24 +1,63 @@
-import http from 'http';
-import express from 'express'
-import morgan from 'morgan'
-import bodyParser from 'body-parser';
+import cron from 'node-cron'
+import request from 'request-promise-native'
 
-import api from './api'
-import crons from './crons'
+import shell from 'shelljs'
 
-let app = express()
-app.server = http.createServer(app);
+function schedule({ name, freq, fn, runImmediately = false }) {
+  const execWrapper = () => {
+    console.log(`Executing task: ${name} (${freq})`)
+    fn()
+  }
 
-app.use(morgan('dev'))
-app.use(bodyParser.json())
+  cron.schedule(freq, execWrapper)
 
-app.use('/', api);
+  if (runImmediately) execWrapper()
+}
 
+const crons = () => {
+  console.log('Initializing cronjobs...')
+
+  schedule({
+    name: 'Load Job State',
+    freq: '*/10 * * * * *',
+    fn: run,
+    runImmediately: true
+  })
+}
+
+async function getState() {
+  return JSON.parse(
+    await request.get('https://dollar-gpu-club-api.herokuapp.com/state')
+  ).state
+}
+
+async function startJob(job) {
+  const aBadIdea = `JOB_ID=${job.id} ${job.command}`
+  console.log(aBadIdea)
+  shell.exec(aBadIdea)
+}
+
+const run = async () => {
+  const state = await getState()
+  const { jobs } = state
+
+  const startableJobs = jobs.filter(
+    j => j.state === 'PENDING' || j.state === 'HALTED'
+  )
+  if (startableJobs.length > 0) {
+    console.log(`Starting ${startableJobs.length} jobs...`)
+
+    await Promise.all(
+      startableJobs.map(async job => {
+        await startJob(job)
+      })
+    )
+
+    console.log('Done starting jobs.')
+  } else {
+    console.log('No startable jobs found.')
+  }
+}
+
+console.log('Running daemon...')
 crons()
-
-app.server.listen(process.env.PORT || 8000, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Running on: http://localhost:${app.server.address().port}`);
-});
-
-export default app
